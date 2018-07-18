9b2fef3b95b686948186982e18399e0f0e79c0f4
mattjgw <mattjgw@gmail.com>
2018-07-18 19:31:59 -0400

    Now reads all test CSVs from directory

--- Linear Regression Expectation.py
+++ Linear Regression Expectation.py
@@ -1,22 +1,25 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import csv
+import os
 
 train_input = list()
 train_output = list()
 
-filename = 'Winning Percentage vs Run Differential.csv'
-
-with open(filename, 'r') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        if row[0] == 'data':
-            train_input.append([int(row[2]), int(row[3])])
-            train_output.append(float(row[1]))
+for file in os.listdir('Testing Data'):
+    if file.endswith('.csv'):
+        rout = 'Testing Data/' + file
+        print(rout)
+        with open(rout, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if row[0] == 'data':
+                    train_input.append([int(row[2]), int(row[3])])
+                    train_output.append(float(row[1]))
 
 predictor = LinearRegression(n_jobs=-1)
 predictor.fit(X=train_input, y=train_output)
 
-x_test = [[693, 784]]
+x_test = [[693, 784]] # Perfect prediction would yield .469, pythagorean expectation is .439
 outcome = predictor.predict(X=x_test)
 print(outcome)
