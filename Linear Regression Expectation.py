1a7a7fd00f00fefc0d92904727b9d108e4b0b3ad
mattjgw <mattjgw@gmail.com>
2018-07-20 22:03:34 -0400

    Now calculating and comparing each model's results

--- Linear Regression Expectation.py
+++ Linear Regression Expectation.py
@@ -23,6 +23,10 @@ for file in os.listdir('Testing Data'):
 predictor = LinearRegression(n_jobs=-1)
 predictor.fit(X=train_input, y=train_output)
 
+pythagorean_results = list()
+linear_regression_results = list()
+actual_results = list()
+
 with open('2017 Winning Percentage vs Run Differential.csv', 'r') as csv_file:
     reader = csv.reader(csv_file)
     for row in reader:
@@ -37,12 +41,30 @@ with open('2017 Winning Percentage vs Run Differential.csv', 'r') as csv_file:
             print('The model predicted: ' + str(outcome))
 
             if abs(float(py_expectation) - float(row[1])) > abs(float(outcome) - float(row[1])):
-                print('My program is more accurate')
+                print('In this case, the linear regression model is more accurate')
             elif abs(float(py_expectation) - float(row[1])) < abs(float(outcome) - float(row[1])):
-                print('Pythagorean expectation is more accurate')
+                print('In this case, the pythagorean expectation is more accurate')
             else:
                 print('This is highly unlikely')
 
-# x_test = [[693, 784]]  # Perfect prediction would yield .469, pythagorean expectation is .439
-# outcome = predictor.predict(X=x_test)
-# print(outcome)
+            pythagorean_results.append(float(py_expectation))
+            linear_regression_results.append(float(outcome))
+            actual_results.append(float(row[1]))
+
+pythagorean_error = list()
+linear_regression_error = list()
+
+for i in range(len(actual_results)):
+    pythagorean_error.append(abs(actual_results[i] - pythagorean_results[i]))
+    linear_regression_error.append(abs(actual_results[i] - linear_regression_results[i]))
+
+pythagorean_average_error = sum(pythagorean_error) / len(pythagorean_error)
+linear_regression_average_error = sum(linear_regression_error) / len(linear_regression_error)
+
+print()
+print('The average error in the pythagorean expectation is ' + str(pythagorean_average_error*100) + ' percent')
+print('The average error in the linear regression prediction is ' + str(linear_regression_average_error*100)
+      + ' percent')
+
+print('The linear regression prediction is ' + str(100 - (linear_regression_average_error/pythagorean_average_error*100))
+      + ' percent more accurate')
